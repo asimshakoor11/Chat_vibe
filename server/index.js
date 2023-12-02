4cc0dbb37adff95f55e96f986a8a37cdcde61a00
Muhammad Asim Shakoor <asimshakoor920830@gmail.com>
2023-12-02 16:20:25 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/userRoutes")
 const messageRoute = require("./routes/messagesRoute")
 const app = express();
 const socket = require("socket.io");
+const port = 5000;
 require("dotenv").config();
 
 app.use(cors({
@@ -30,8 +31,8 @@ mongoose.connect("mongodb+srv://asim119913:AsimS119913@chatapp.jbwoqag.mongodb.n
 
 // connectDB();
 
-const server = app.listen( () => {
-  console.log("Server started on port 5000");
+const server = app.listen(port, () => {    
+        console.log(`Server started on port ${port}`);
 });
 
 const io = socket(server,{
