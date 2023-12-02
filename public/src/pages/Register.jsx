9338ad7981cfe70794139f61255a5d69947300ef
Muhammad Asim Shakoor <asimshakoor920830@gmail.com>
2023-12-02 18:38:32 +0500

    Update Register.jsx

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -112,7 +112,7 @@ function Register() {
           />
           <button type="submit">Create User</button>
           <span>
-            Already have and account ? <Link to="/login">Login</Link>
+            Already have an account ? <Link to="/login">Login</Link>
           </span>
         </form>
       </FormContainer>
