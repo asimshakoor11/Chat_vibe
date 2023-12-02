c5118bbc80c2ebea7d5b0863d2ee3a3e1119402c
Muhammad Asim Shakoor <asimshakoor920830@gmail.com>
2023-12-02 20:00:09 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ const socket = require("socket.io");
 const port = 5000;
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://chat-vibe-sigma.vercel.app/"],
+        method: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
