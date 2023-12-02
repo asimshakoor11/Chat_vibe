db1a802e7b1dcbb9da89348cc0db9ba93f3cb6ec
Muhammad Asim Shakoor <asimshakoor920830@gmail.com>
2023-12-02 17:46:31 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const messageRoute = require("./routes/messagesRoute")
 const app = express();
 const socket = require("socket.io");
 const port = 5000;
-require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
