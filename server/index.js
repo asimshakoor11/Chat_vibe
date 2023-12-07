d0bddfc745c3a64944f48f99c1143817f4488936
Muhammad Asim Shakoor <asimshakoor920830@gmail.com>
2023-12-07 18:54:03 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,19 +8,19 @@ const socket = require("socket.io");
 const port = 5000;
 require("dotenv").config();
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://chat-vibe-sigma.vercel.app');
-//   // Add other headers as needed
-//   // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://chat-vibe-sigma.vercel.app');
+  // Add other headers as needed
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
-app.use(cors({
-    origin: 'https://chat-vibe-sigma.vercel.app',
-    method: ["POST", "GET"],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: 'https://chat-vibe-sigma.vercel.app',
+//     method: ["POST", "GET"],
+//     credentials: true
+// }));
 
 app.use(express.json());
 
@@ -42,7 +42,7 @@ const server = app.listen(port, () => {
         console.log(`Server started on port ${port}`);
 });
 
-const socket = io("ws://chat-vibe-sigma.vercel.app", {
+const socket1 = io("ws://chat-vibe-sigma.vercel.app", {
   transports: ["websocket"]
 });
 // const io = socket(server,{
@@ -54,7 +54,7 @@ const socket = io("ws://chat-vibe-sigma.vercel.app", {
 
 global.onlineUsers = new Map();
 
-io.on("connection", (socket) => {
+socket1.on("connection", (socket) => {
     global.chatSocket = socket;
     socket.on("add-user", (userId) => {
         onlineUsers.set(userId, socket.id);
