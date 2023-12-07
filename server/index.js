a11e92f9c4414fdce4cf06d2906931c155ef9db7
Muhammad Asim Shakoor <asimshakoor920830@gmail.com>
2023-12-07 18:48:39 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,19 +8,20 @@ const socket = require("socket.io");
 const port = 5000;
 require("dotenv").config();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://chat-vibe-sigma.vercel.app');
-  // Add other headers as needed
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://chat-vibe-sigma.vercel.app');
+//   // Add other headers as needed
+//   // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   next();
+// });
 
-// app.use(cors({
-//     origin: 'https://chat-vibe-sigma.vercel.app',
-//     method: ["POST", "GET"],
-//     credentials: true
-// }));
+app.use(cors({
+    origin: 'https://chat-vibe-sigma.vercel.app',
+    method: ["POST", "GET"],
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
